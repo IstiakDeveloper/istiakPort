23bd8bff2e3e3d5d8d4896c30a5e4b94f092b4fb
IstiakDeveloper <istiakhossainofficial@gmail.com>
2022-02-27 18:26:05 +0600

    first commit

---
+++ js/typed/typeCustom.js
@@ -0,0 +1,11 @@
+var typed = new Typed('.type', {
+            strings: [
+                'istiak Hossain',
+                'a Developer',
+                'a Designer',
+                'and Freelancer'
+                ],
+                typeSpeed: 60,
+                backSpeed: 60,
+                loop: true
+            });
